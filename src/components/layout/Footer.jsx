0b77c8bf1bbd5160b7a40c52430651db91e3f493
shukra <shukratamang.npl@gmail.com>
2024-05-20 09:07:09 +1000

    Header defined

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='bg-dark text-light p-5 text-center mt-2'>
+      {' '}
       &copy; Copy right all reserved. Made by <a href=''>Shukra</a>
     </div>
   );
