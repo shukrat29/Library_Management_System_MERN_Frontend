bd9c158056939db45be0dcda80ac65234fd1302f
shukra <shukratamang.npl@gmail.com>
2024-05-21 07:51:31 +1000

    Header defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 import Footer from './components/layout/Footer';
+import { Header } from './components/layout/Header';
 
 function App() {
-  return <Footer></Footer>;
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const Header = () => {
-  return <div>Header</div>;
+export const Header = () => {
+  return (
+    <Navbar expand='md' variant='dark' className='bd-dark'>
+      <Container>
+        <Link to='/' className='navbar-brand'>
+          LM
+        </Link>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ms-auto'>
+            <Link className='nav-link' to='/'>
+              <i className='fa-solid fa-house'></i> Home
+            </Link>
+            <Link className='nav-link' to='/signin'>
+              {' '}
+              <i className='fa-solid fa-right-to-bracket'></i> SignIn
+            </Link>
+            <Link className='nav-link' to='/signup'>
+              <i className='fa-solid fa-user'></i> SignUp
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
-
-export default Header;
