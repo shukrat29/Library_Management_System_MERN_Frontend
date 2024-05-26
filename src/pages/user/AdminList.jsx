33cc752cb3dfbd53dd47bb36a4cf5d5b7ba5c540
shukra <shukratamang.npl@gmail.com>
2024-05-26 22:24:07 +1000

    admin list added

---
+++ src/pages/user/AdminList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { UserLayout } from '../../components/layout/UserLayout';
+
+const AdminList = () => {
+  return <UserLayout pageTitle={'Admin List'}>AdminList</UserLayout>;
+};
+
+export default AdminList;
